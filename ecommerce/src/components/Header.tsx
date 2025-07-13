02e6e94df861f6b072f55c1a2f31224cc714a502
dhakeswor <Santoshneupane17@gmail.com>
2025-07-13 22:55:45 +0300

    adding the header section

--- ecommerce/src/components/Header.tsx
+++ ecommerce/src/components/Header.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+        <h1>Welcome to Our E-commerce Store</h1>
+        <nav>
+            <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/products">Products</a></li>
+            <li><a href="/about">About Us</a></li>
+            <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
   )
 }
 
