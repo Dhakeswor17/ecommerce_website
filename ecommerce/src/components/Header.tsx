73a106a42fd9da9365f93d7af9fab70fcdbac5a2
dhakeswor <Santoshneupane17@gmail.com>
2025-07-14 21:38:05 +0300

    making globalTheme for dark mode

--- ecommerce/src/components/Header.tsx
+++ ecommerce/src/components/Header.tsx
@@ -1,16 +1,12 @@
-import React from 'react'
+
 
 const Header = () => {
   return (
     <div className='header'>
-        <h1>Welcome to Our E-commerce Store</h1>
         <nav>
-            <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/products">Products</a></li>
-            <li><a href="/about">About Us</a></li>
-            <li><a href="/contact">Contact</a></li>
-            </ul>
+            
+            <ul> </ul>
+            <ul> </ul>
         </nav>
     </div>
   )
