1922542fd8c7f4b533ca5e5ec786ce3d4bb3beaf
dhakeswor <Santoshneupane17@gmail.com>
2025-07-28 21:16:41 +0300

    modified the Home page

--- ecommerce/src/Pages/Home.tsx
+++ ecommerce/src/Pages/Home.tsx
@@ -1,26 +1,52 @@
-import React from "react";
-import { Box, Typography, Grid } from "@mui/material";
+import React from 'react';
+import { Grid, Container, Box } from '@mui/material';
+import Banner from '../../components/Banner/Banner';
+import CategoryBar from '../../components/CategoryBar/CategoryBar';
+import ProductCard from '../../components/ProductCard/ProductCard';
 
-import { useSelector } from "react-redux";
-import { RootState } from "../redux/store";
+const demoProducts = [
+  {
+    image: 'https://via.placeholder.com/300x200',
+    title: 'Smartphone XYZ',
+    price: 299.99,
+    originalPrice: 399.99,
+  },
+  {
+    image: 'https://via.placeholder.com/300x200',
+    title: 'Wireless Headphones',
+    price: 99.99,
+    originalPrice: 129.99,
+  },
+  {
+    image: 'https://via.placeholder.com/300x200',
+    title: 'Fitness Tracker',
+    price: 59.99,
+    originalPrice: 89.99,
+  },
+  {
+    image: 'https://via.placeholder.com/300x200',
+    title: 'Smart TV 50"',
+    price: 499.99,
+    originalPrice: 599.99,
+  },
+];
 
-const Home: React.FC = () => {
-  const products = useSelector((state: RootState) => state.product.items);
-
+const Home = () => {
   return (
-    <Box>
-      <HeroBanner />
-      <CategoryBar />
-      <Typography variant="h5" fontWeight={600} mt={4} ml={2}>
-        Recommended for You
-      </Typography>
-      <Grid container spacing={2} px={2} mt={1}>
-        {products.map((product) => (
-          <Grid item xs={6} sm={4} md={3} key={product.id}>
-            <ProductCard product={product} />
-          </Grid>
-        ))}
-      </Grid>
+    <Box className="home-page" sx={{ backgroundColor: '#121212', color: '#fff', py: 4 }}>
+      <Container>
+        <Banner />
+        <Box my={3}>
+          <CategoryBar />
+        </Box>
+        <Grid container spacing={3}>
+          {demoProducts.map((product, index) => (
+            <Grid item xs={12} sm={6} md={3} key={index}>
+              <ProductCard {...product} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </Box>
   );
 };
