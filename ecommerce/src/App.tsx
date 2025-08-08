2b29bd6eaa9e6cf9fa4e45e15b0bc1cc924b97bb
dhakeswor <Santoshneupane17@gmail.com>
2025-08-08 21:32:36 +0300

    adding route in App

--- ecommerce/src/App.tsx
+++ ecommerce/src/App.tsx
@@ -1,12 +1,43 @@
-import Header from "./components/Header/Header";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
+import Home from './pages/Home/Home';
+import ProductDetail from './pages/ProductDetail/ProductDetail';
+import Cart from './pages/Cart/Cart';
+import Checkout from './pages/Checkout/Checkout';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+import Header from './components/Header/Header';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    background: {
+      default: '#121212',
+    },
+    primary: {
+      main: '#00ADB5',
+    },
+  },
+});
 
-const App = () => {
+function App() {
   return (
-    
-    <Route path="/login" element={<Login />} />
-<Route path="/register" element={<Register />} />
-  )
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
