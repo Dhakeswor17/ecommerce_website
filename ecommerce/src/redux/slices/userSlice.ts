369d90901788d19a914175f03c016c0946fbce0a
dhakeswor <Santoshneupane17@gmail.com>
2025-08-09 18:45:18 +0300

    Redux for User Authentication so we can handle login, register, and logout state globally.

---
+++ ecommerce/src/redux/slices/userSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  token: string;
+};
+
+type UserState = {
+  user: User | null;
+};
+
+const initialState: UserState = {
+  user: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    loginSuccess: (state, action: PayloadAction<User>) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { loginSuccess, logout } = userSlice.actions;
+export default userSlice.reducer;
