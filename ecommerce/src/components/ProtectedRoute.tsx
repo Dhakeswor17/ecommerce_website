862ef99c979b8236e41a4b9c231fa7908316db25
dhakeswor <Santoshneupane17@gmail.com>
2025-08-10 17:46:16 +0300

    ProtectedRoute components

---
+++ ecommerce/src/components/ProtectedRoute.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+import { RootState } from '../redux/store';
+
+const ProtectedRoute = () => {
+  const user = useSelector((s: RootState) => s.user.user);
+  return user ? <Outlet /> : <Navigate to="/login" replace />;
+};
+
+export default ProtectedRoute;
