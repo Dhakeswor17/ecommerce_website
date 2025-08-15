4488cf1d5ae866d0e3ef4dacde67b7f9c4ad8364
dhakeswor <Santoshneupane17@gmail.com>
2025-08-15 21:05:02 +0300

    fixing the import

--- ecommerce/src/Pages/Home.tsx
+++ ecommerce/src/Pages/Home.tsx
@@ -1,26 +1,56 @@
 // src/pages/Home/Home.tsx
 import React from 'react';
 import { Grid, Container, Box, Typography } from '@mui/material';
-import Banner from '../../components/Banner/Banner';
-import CategoryBar from '../../components/CategoryBar/CategoryBar';
-import ProductCard from '../../components/ProductCard/ProductCard';
+import Banner from '../components/Banner/Banner';
+import CategoryBar from '../components/CategoryBar/CategoryBar';
+import ProductCard from '../components/ProductCard/ProductCard';
 import { useSelector } from 'react-redux';
-import { selectFilteredProducts } from '../../redux/slices/productSlice';
+import { selectFilteredProducts, selectRecentlyViewed } from '../redux/slices/productSlice';
 
-const Home = () => {
+const Home: React.FC = () => {
   const products = useSelector(selectFilteredProducts);
+  const recent = useSelector(selectRecentlyViewed);
 
   return (
-    <Box className="home-page" sx={{ backgroundColor: '#121212', color: '#fff', py: 4 }}>
+    <Box sx={{ backgroundColor: '#121212', color: '#fff', py: 4 }}>
       <Container>
+        {/* Hero */}
         <Banner />
+
+        {/* Categories */}
         <Box my={3}>
           <CategoryBar />
         </Box>
 
-        {products.length === 0 ? (
-          <Typography>No products match your search.</Typography>
-        ) : (
+        {/* Recently viewed */}
+        {recent.length > 0 && (
+          <>
+            <Box mt={2} mb={1}>
+              <Typography variant="h6">Recently viewed</Typography>
+            </Box>
+            <Grid container spacing={3} sx={{ mb: 4 }}>
+              {recent.map((p) => (
+                <Grid item xs={12} sm={6} md={3} key={`recent-${p.id}`}>
+                  <ProductCard
+                    id={p.id}
+                    image={p.image}
+                    title={p.title}
+                    price={p.price}
+                    originalPrice={p.originalPrice}
+                  />
+                </Grid>
+              ))}
+            </Grid>
+          </>
+        )}
+
+        {/* Product grid (filtered by search/category) */}
+        <Box mt={2} mb={1}>
+          <Typography variant="h6">
+            {products.length > 0 ? 'Products' : 'No products match your search.'}
+          </Typography>
+        </Box>
+        {products.length > 0 && (
           <Grid container spacing={3}>
             {products.map((p) => (
               <Grid item xs={12} sm={6} md={3} key={p.id}>
