cccf510cbfd64c2f58cd5300ddd069e9d102bd70
dhakeswor <Santoshneupane17@gmail.com>
2025-08-20 23:34:58 +0300

    fixnig thr payload

--- ecommerce/src/redux/slices/wishlistSlice.ts
+++ ecommerce/src/redux/slices/wishlistSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 
 type WishlistState = {
   ids: string[]; // product ids
